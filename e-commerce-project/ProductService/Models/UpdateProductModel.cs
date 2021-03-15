4b3efe912222d631b6c62bf1f35234c605550108
unknown <akin.isik@windowslive.com>
2021-03-15 12:10:11 +0300

    added name in updateproductmodel

--- e-commerce-project/ProductService/Models/UpdateProductModel.cs
+++ e-commerce-project/ProductService/Models/UpdateProductModel.cs
@@ -7,6 +7,7 @@ namespace ProductService.Models
 {
     public class UpdateProductModel
     {
+        public string Name { get; set; }
         public string Barcode { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
